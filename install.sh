f1106a5add88fa32dcf3b91e562cef4ff519a360
luke <danielsonlukea@gmail.com>
2021-04-01 23:22:30 -0700

    added nvim

--- install.sh
+++ install.sh
@@ -1,33 +1,45 @@
 #! /usr/bin/env bash
 
-# installs all of the bash dependencies
-dependencies=("stow" "zsh" "oh-my-zsh")
+dependencies=("stow" "zsh" "oh-my-zsh" "nvim")
 
 function install() {
  case $1 in
  stow)
    apt-get install stow
  ;;
- 
  zsh)
    apt-get install zsh
  ;;
  oh-my-zsh)
    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
  ;;
+ nvim)
+  apt-get install neovim
+  mkdir ~/.config/nvim/ -p
+  touch ~/.config/nvim/init.vim
+  echo "set runtimepath+=~/.vim,~/.vim/after
+        set packpath+=~/.vim
+        source ~/.vimrc" >> ~/.config/nvim/init.vim 
+ ;;
  esac
 }
 
-for t in ${dependencies[@]}; do
-
-  dpkg-query --show $t
-  PACKAGE_RETURN_CODE=$? 
+if [ $# -eq 0 ];then
+  for t in ${dependencies[@]}; do
   
-  if [ "$PACKAGE_RETURN_CODE" = "0" ]; then
-    echo "dependency satisfied $t"
-    continue
-  else
-    echo "must download package $t"
-    install $t
-  fi
-done
+    dpkg-query --show $t
+    PACKAGE_RETURN_CODE=$? 
+    
+    if [ "$PACKAGE_RETURN_CODE" = "0" ]; then
+      echo "dependency satisfied $t"
+      continue
+    else
+      echo "must download package $t"
+      install $t
+    fi
+  done
+else
+ for var in "$@"; do
+   install $var
+ done
+fi
