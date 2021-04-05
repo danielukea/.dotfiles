7d7f5cd80af6ca2159a4f28a0c511b67784cc82a
luke <danielsonlukea@gmail.com>
2021-04-04 22:40:56 -0700

    added install_scripts folder

--- destow_links.sh
+++ destow_links.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 for d in * ; do
+ if [ "$d" == "install_scripts" ]; then continue; fi
+
  if [ -d "$d" ]; then
   stow -D -v $d
  fi
--- stow_links.sh
+++ stow_links.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 
 for d in * ; do
+ if [ "$d" == "install_scripts" ]; then
+     continue
+ fi
+
  if [ -d "$d" ]; then
   stow $d -v
- fi 
+ fi
 done
