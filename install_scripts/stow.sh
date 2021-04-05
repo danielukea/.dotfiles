d3b7ff8f4b2598f15b2476b96b22a61b2d2746b3
luke <danielsonlukea@gmail.com>
2021-04-04 23:53:50 -0700

    consolidated apt dependencies to one file

--- install_scripts/stow.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-if which stow >/dev/null; then
-    echo "stow is already installed"
-else
-    "installing stow..."
-     apt-get install stow && echo "Stow successfully installed"
-fi
