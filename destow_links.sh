96c3002f55e472cac963dc9eb4fb18baaac3b629
luke <danielsonlukea@gmail.com>
2021-04-11 17:14:09 -0700

    added stow for .config/nvim/init.vim and destow

--- destow_links.sh
+++ destow_links.sh
@@ -3,6 +3,11 @@
 for d in * ; do
  if [ "$d" == "install_scripts" ]; then continue; fi
 
+ if [ "$d" == "nvim" ]; then
+     stow -D nvim -t ~/.config
+     continue
+ fi
+
  if [ -d "$d" ]; then
   stow -D -v $d
  fi
