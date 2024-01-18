d1ffaafdd1555696fc8051de94544392f166813c
Luke Danielson <lukedanielson@Lukes-MacBook-Pro.local>
2024-01-18 11:28:40 -0700

    removed all the ubuntu stuff

--- install_scripts/asdf.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-if [[ -d ~/.asdf ]]; then
-    echo "asdf has already been installed. Check your ~/.asdf directory"
-else
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
-fi
