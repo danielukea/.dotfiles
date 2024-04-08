be0284156199853f333be0a839b7441b20eea5a6
Luke Danielson <lukedanielson@biggerpockets.com>
2024-04-08 15:31:40 -0600

    Added raycast and shortcat

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -3,8 +3,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        eruby = { "htmlbeautifier" },
-        html = { "htmlbeautifier" },
+        --eruby = { "htmlbeautifier" },
+        --html = { "htmlbeautifier" },
         javascript = { "prettierd", "prettier" },
       },
     },
