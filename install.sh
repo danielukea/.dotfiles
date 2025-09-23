dec0a7a7a7f97b15ab96f1efc3768460e043acd5
Luke Danielson <luke.danielson@wealthbox.com>
2025-09-23 15:41:05 -0600

    feat: added more ruby dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+set -e  # Exit on any error
+
 echo "Installing the .dotfiles"
 echo "STEP 1: cleanup existing stow links"
 ./link.sh unlink
@@ -10,14 +12,6 @@ pushd $HOME
 echo "STEP 3: install all brew files"
 brew bundle install
 
-echo "STEP 4: install asdf plugin manager"
-asdf plugin add asdf-plugin-manager https://github.com/asdf-community/asdf-plugin-manager.git
-asdf install asdf-plugin-manager latest
-
-echo "STEP 5: install all asdf plugins"
-asdf-plugin-manager add-all
-
-echo "STEP 6: install all asdf dependencies"
-asdf install
-
 popd
+
+echo "✅ Dotfiles installation complete!"
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,12 @@
 
 vim.opt.winbar = "%=%m %f"
 vim.api.nvim_create_user_command("CopyRelPath", "call setreg('+', expand('%'))", {})
+
+-- Tree-sitter configuration
+-- Disable tree-sitter for problematic file types if needed
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "vim", "help" },
+  callback = function()
+    vim.treesitter.stop()
+  end,
+})
